c2f7a946eac973d5a62e9adfc2acbd7bbcfb8a8b
Rashidbek <rasishidbekrasuljonov83>
2026-04-09 22:37:37 +0300

    The academic publication project is ready

---
+++ main.js
@@ -0,0 +1,96 @@
+//Uch xonali son berilgan. Oldin uni birliklar xonasidagi raqamni so`ng o`nliklar xonasidagi raqamni chiqaruvchi programma tuzilsin.
+
+// let n = 333;
+
+// let onlar = Math.floor((n / 10) % 10);
+// let birlar = n % 10;
+
+// console.log("onlar", onlar);
+// console.log("birlar", birlar);
+
+//<==================================================================================================
+
+//Integer2. Uch xonali son berilgan. Uning raqamlar yig`indisini aniqlovchi programma tuzilsin.
+
+// let n = 768;
+
+// let a = Math.floor(n / 100);
+// let b = Math.floor((n / 10) % 10);
+// let c = n % 10;
+// let yigindisi = a + b + c;
+// console.log("Raqamlar yig'indisi: " + yigindisi);
+
+//<==================================================================================================
+
+//Integer3. Uch xonali son berilgan. Uning raqamlarini teskari tartibda yozishdan hosil bo`lgan sonni aniqlovchi program tuzilsin.
+
+// let son = 546;
+// let a = Math.floor(son / 100);
+// let b = Math.floor((son / 10) % 10);
+// let c = son % 10;
+// let teskarison = c * 100 + b * 10 + a;
+// console.log("teskari son", teskarison);
+
+//<==================================================================================================
+
+// Uch xonali son berilgan. Uning o`nliklar xonasidagi raqam bilan yuzliklar xonasidagi raqamni almashtirishdan hosil bo`lgan sonni aniqlovchi programma tuzilsin. Input: 123   Output: 213
+
+// let son = 890;
+// let a = Math.floor(son / 100);
+// let b = Math.floor((son / 10) % 10);
+// let c = son % 10;
+// let javob = b * 100 + a * 10 + c;
+// console.log(javob);
+
+//<==================================================================================================
+
+//Uch xonali son berilgan. Uning o`nliklar xonasidagi raqam bilan birliklar xonasidagi raqamni almashtirishdan hosil bo`lgan sonni aniqlovchi programma tuzilsin.   Input: 123   Output: 132
+
+// let son = 547;
+// let a = Math.floor(son / 100);
+// let b = Math.floor((son / 10) % 10);
+// let c = son % 10;
+// let javobi = a * 100 + c * 10 + b;
+// console.log(javobi);
+
+//<==================================================================================================
+
+//999 dan katta bo`lgan son berilgan. Bir marta bo`lib butunni va bo`lib qoldiqni olish operatsiyasidan foydalanib berilgan sonni mingliklar xonasidagi sonni aniqlovchi programma tuzilsin.
+
+// let son = 98123;
+// let a = Math.floor(son / 1000) % 10;
+// console.log(a);
+
+//<==================================================================================================
+
+//Kun boshidan boshlab N sekund vaqt o'tti. Kun boshidan boshlab qancha minut to'la o`tganligini aniqlovchi programma tuzilsin.
+
+// let n = 12345;
+// let utgan_Minut = Math.floor(n / 60);
+// console.log(`Kun boshidan boshlab   ${utgan_Minut}   minut to'la o'tdi.`);
+
+//<==================================================================================================
+
+// Kun boshidan boshlab N sekund vaqt o'tti. Kun boshidan boshlab qancha to'la soat o`tganligini aniqlovchi programma tuzilsin.
+
+// let h = 12345;
+// let tula_vaqt = Math.floor(h / 3600);
+// console.log(`Kun boshidan boshlab ${tula_vaqt} soat to'la o'tdi.`);
+
+//<==================================================================================================
+
+// Kun boshidan boshlab N sekund vaqt o`tti. Kun boshidan boshlab qancha minut va sekund o'tganini aniqlovchi programma tuzilsin.
+
+// let N = 350;
+// let a = Math.floor(N / 60);
+// let b = N % 60;
+// console.log(`Kun boshidan boshlab   ${a}   minut va  ${b} sekund o'tdi.`);
+
+//<==================================================================================================
+
+//Kun boshidan boshlab N sekund vaqt o`tti. Kun boshidan boshlab qancha soat va sekund o'tganini aniqlovchi programma tuzilsin.
+
+// let N = 12345;
+// let a = Math.floor(N / 3600);
+// let b = N % 60;
+// console.log(`Kun boshidan boshlab ${a} soat va ${b} sekund o'tdi.`);
